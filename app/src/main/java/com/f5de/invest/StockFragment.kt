5ab45c614f51acff619cc27157e514306e4c9462
Yan <stranayan@gmail.com>
2021-02-02 21:15:10 +0300

    [UI] + added stock card, adjusted fragments

--- app/src/main/java/com/f5de/invest/StockFragment.kt
+++ app/src/main/java/com/f5de/invest/StockFragment.kt
@@ -5,27 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import android.widget.LinearLayout
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class StockFragment : Fragment() {
+
+    private lateinit var stockCardHolder: LinearLayout
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_stock, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        stockCardHolder = view.findViewById(R.id.stock_card_holder) as LinearLayout
+        val card = layoutInflater.inflate(R.layout.card_stock, null)
+        stockCardHolder.addView(card, 0)
+//        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
     }
 }
